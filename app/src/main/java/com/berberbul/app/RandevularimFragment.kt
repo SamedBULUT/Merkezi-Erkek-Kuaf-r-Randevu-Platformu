afc6033ce87a77f516707688369fd18b644d01db
SamedBULUT <smdblt3603@gmail.com>
2026-05-03 17:57:13 +0300

    Randevularım sayfası eklendi ve firebase verileri listelendi.

--- app/src/main/java/com/berberbul/app/RandevularimFragment.kt
+++ app/src/main/java/com/berberbul/app/RandevularimFragment.kt
@@ -1,59 +1,57 @@
 package com.berberbul.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RandevularimFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RandevularimFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var rvRandevularim: RecyclerView
+    private lateinit var randevuAdapter: RandevuAdapter
+    private val randevuListesi = mutableListOf<Randevu>()
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_randevularim, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RandevularimFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RandevularimFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvRandevularim = view.findViewById(R.id.rvRandevularim)
+        rvRandevularim.layoutManager = LinearLayoutManager(requireContext())
+
+        randevuAdapter = RandevuAdapter(randevuListesi)
+        rvRandevularim.adapter = randevuAdapter
+
+        randevulariGetir()
+    }
+
+    private fun randevulariGetir() {
+        db.collection("randevular")
+            .whereEqualTo("customerName", "Müşteri Can")
+            .get()
+            .addOnSuccessListener { documents ->
+                randevuListesi.clear()
+                for (document in documents) {
+                    val randevu = document.toObject(Randevu::class.java)
+                    randevuListesi.add(randevu)
                 }
+                randevuAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("BackendTest", "Randevular çekilemedi", exception)
+                Toast.makeText(requireContext(), "Randevular yüklenemedi.", Toast.LENGTH_SHORT).show()
             }
     }
 }
